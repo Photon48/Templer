8409c68234a853b4fde18b1c36eed0c4f862aaf9
Photon48 <48671579+Photon48@users.noreply.github.com>
2023-08-04 14:58:57 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ from langchain.schema import (
 )
 API_KEY = os.getenv('PROJECT_API_KEY')
 
-openai_api_key = os.getenv("OPENAI_API_KEY", "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")
-os.environ['OPENAI_API_KEY'] = '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'
+openai_api_key = os.getenv('OPENAI_API_KEY')
 nltk.download('averaged_perceptron_tagger')
 
 
